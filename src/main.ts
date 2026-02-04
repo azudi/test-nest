31dbe3bfe56c89ad1bef2858eb08ad7784135113
azudi001 <118183047+azudi001@users.noreply.github.com>
2026-02-04 18:31:41 +0100

    bug: revert support ticket route link

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   await app.listen(port, '0.0.0.0'); 
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 
   // loggerService.log(
   //   `Application is running on: ${await app.getUrl()}`,
